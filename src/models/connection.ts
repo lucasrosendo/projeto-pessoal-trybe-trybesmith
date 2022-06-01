ab50419112a6ace52357196c5eb2a2ad0435d333
lucasrosendo <lucasrosendo91@gmail.com>
2022-06-01 19:18:19 -0300

    getAll products

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,6 +1,5 @@
-import mysql from 'mysql2/promise';
-
 import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
 
 dotenv.config();
 
@@ -8,5 +7,4 @@ export default mysql.createPool({
   host: process.env.DB_HOSTNAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
 });
