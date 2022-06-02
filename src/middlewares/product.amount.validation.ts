ee9303af1de82469af1230ae92b5709eb1e7b919
lucasrosendo <lucasrosendo91@gmail.com>
2022-06-02 13:42:10 -0300

    validations

---
+++ src/middlewares/product.amount.validation.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validateProductAmount = (req: Request, res: Response, next: NextFunction) => {
+  const { amount } = req.body;
+  if (amount === undefined) {
+    return res.status(400).json({ message: '"amount" is required' });
+  }
+  if (typeof amount !== 'string') {
+    return res.status(422).json({ message: '"amount" must be a string' });
+  }
+  if (amount.length < 3) {
+    return res.status(422).json({ message: '"amount" length must be at least 3 characters long' });
+  }
+  next();
+};
+export default validateProductAmount;
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import validateProductName from '../middlewares/product.name.validation';
+import validateProductAmount from '../middlewares/product.amount.validation';
 
 const router = Router();
 
 const productsController = new ProductsController();
 
 router.get('/', productsController.getAll);
-router.post('/', productsController.create);
+router.post('/', validateProductName, validateProductAmount, productsController.create);
 
 export default router;
